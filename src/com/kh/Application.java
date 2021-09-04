1221ac9b5431ce7dc62a35aca79ace48eac8faea
jinn13 <75795444+jinn13@users.noreply.github.com>
2021-09-04 16:21:51 +0900

    Add files via upload

---
+++ src/com/kh/Application.java
@@ -0,0 +1,40 @@
+package com.kh.array;
+
+import com.kh.array.practice.C_ArraySort;
+
+public class Application {
+
+	public static void main(String[] args) {
+//		new A_Array().method1();
+//		new A_Array().method2();
+//		new A_Array().method3();
+//		new A_Array().method4();
+
+//		new B_ArrayCopy().method1();
+//		new B_ArrayCopy().method2();
+//		new B_ArrayCopy().method3();
+//		new B_ArrayCopy().method4();
+//		new B_ArrayCopy().method5();
+		
+//		new C_ArraySort().method1();
+		new C_ArraySort().method2();
+		
+//		ArrayPractice ap=new ArrayPractice();
+//		ap.practice1();
+//		ap.practice2();
+//		ap.practice3();
+//		ap.practice4();
+//		ap.practice5();
+//		ap.practice6();
+//		ap.practice7();
+//		ap.practice8();
+//		ap.practice9();
+//		ap.practice10();
+//		ap.practice11();
+//		ap.practice12();
+//		ap.practice13();
+		
+		
+	}
+
+}
