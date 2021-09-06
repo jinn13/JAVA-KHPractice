deb97333a3e791320e1ad5e3d5bed3a3906d8d75
jinn13 <75795444+jinn13@users.noreply.github.com>
2021-09-07 01:43:50 +0900

    Add files via upload

--- src/com/kh/object/Application.java
+++ src/com/kh/object/Application.java
@@ -1,47 +1,37 @@
 package com.kh.object;
 
-import com.kh.object.practice.Product;
+import com.kh.object.practice.Book;
 
 public class Application {
 
 	public static void main(String[] args) {
-		// 매개변수 생성자를 이용하여 3개의 객체 생성(위의 사용데이터 참고)
+		// 기본생성자를 이용하여 첫 번째 Book 객체 생성
+		Book book1=new Book();
+		// 매개변수 생성자를 이용하여 두 번째 Book 객체 생성
+		Book book2=new Book("자바의정석", 20000, 0.2, "윤상섭");
 		// 객체가 가진 필드 값 출력 확인
-		Product phone1=new Product("ssgnote9", "갤럭시노트9", "경기도 수원", 960000, 10.0);
-		Product phone2=new Product("lgxnote5", "LG스마트폰5", "경기도 평택", 780000, 0.7);
-		Product phone3=new Product("ktsnote3", "KT스마트폰3", "서울시 강남", 250000, 0.3);
+		System.out.println(book1.information());
+		System.out.println(book2.information());
+		System.out.println("==============================");
 		
-//		phone1.Product("ssgnote9", "갤럭시노트9", "경기도 수원", 960000, 10.0);
-//		phone2.Product("lgxnote5", "LG스마트폰5", "경기도 평택", 780000, 0.7);
-//		phone3.Product("ktsnote3", "KT스마트폰3", "서울시 강남", 250000, 0.3);
-
-		System.out.println(phone1.information());
-		System.out.println(phone2.information());
-		System.out.println(phone3.information());
-		System.out.println("=====================================================");
+		// 첫 번째 객체가 가진 값을 setter를 이용하여 수정
+		book1.setTitle("C언어");
+		book1.setprice(30000);
+		book1.setDiscountRate(0.1);
+		book1.setAuthor("홍길동");
 		
-		// 각 객체의 가격을 모두 120만원으로 변경/부가세율도 모두 0.05로 변경
-		// 객체가 가진 필드 값 출력 확인
-		phone1.setPrice(1200000);
-		phone2.setPrice(1200000);
-		phone3.setPrice(1200000);
-		phone1.setTax(0.05);
-		phone2.setTax(0.05);
-		phone3.setTax(0.05);
-	
-		System.out.println(phone1.information());
-		System.out.println(phone2.information());
-		System.out.println(phone3.information());
-		System.out.println("=====================================================");
+		// 수정된 내용 출력 확인
+		System.out.println("수정된 결과 확인");
+		System.out.println(book1.information());
+		System.out.println("==============================");
 		
-		//각 객체의 가격에 부가세율을 적용한 실제 가격을 계산해서 출력함
-		// 실제가격=가격+(가격*부가세율)
-		
-		System.out.print("상품 = "+phone1.getName()+"\n부가세 포함 가격 = "+
-						((int)(phone1.getPrice()+(phone1.getPrice()*phone1.getTax()))+"원"));
-		System.out.print("\n상품 = "+phone2.getName()+"\n부가세 포함 가격 = "+
-						((int)(phone2.getPrice()+(phone2.getPrice()*phone2.getTax()))+"원"));
-		System.out.print("\n상품 = "+phone3.getName()+"\n부가세 포함 가격 = "+
-						((int)(phone3.getPrice()+(phone3.getPrice()*phone3.getTax()))+"원"));
+		// 각 객체의 할인율을 적용한 책 가격을 계산해서 출력
+		// 할인된 가격 = 가격 - (가격 * 할인율)
+		System.out.println("도서명 = "+book1.getTitle());
+		System.out.println("할인된 가격 = "+(int)(book1.getprice()-
+				(book1.getprice()*book1.getDiscountRate()))+"원");
+		System.out.println("도서명 = "+book2.getTitle());
+		System.out.println("할인된 가격 = "+(int)(book2.getprice()-
+				(book1.getprice()*book1.getDiscountRate()))+"원");
 	}
 }
